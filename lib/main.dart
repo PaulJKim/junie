f9ebbf07c767565eb6cf663ea366fa57d367fe89
Paul Kim <pauljk704@gmail.com>
2022-09-29 22:50:29 -0400

    add dialog and improve styling

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Has Juni eaten today?'),
@@ -30,7 +21,12 @@ class MyApp extends StatelessWidget {
 }
 
 class MealStatusBar extends StatefulWidget {
-  const MealStatusBar({super.key});
+  const MealStatusBar({super.key, required this.breakfastServed, required this.dinnerServed, this.breakfastServer = '', this.dinnerServer = ''});
+
+  final String breakfastServer;
+  final bool breakfastServed;
+  final String dinnerServer;
+  final bool dinnerServed;
 
   @override
   State<MealStatusBar> createState() => _MealStatusBarState();
@@ -41,18 +37,20 @@ class _MealStatusBarState extends State<MealStatusBar> {
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: const [
-        MealStatus(mealName: "Breakfast", served: false),
-        MealStatus(mealName: "Dinner", served: true)
+      children: [
+        MealStatus(mealName: "Breakfast", served: widget.breakfastServed, server: widget.breakfastServer,),
+        const SizedBox(width: 20),
+        MealStatus(mealName: "Dinner", served: widget.dinnerServed, server: widget.dinnerServer,)
       ],
     );
   }
 }
 
 class MealStatus extends StatefulWidget {
-  const MealStatus({super.key, required this.mealName, required this.served});
+  const MealStatus({super.key, required this.mealName, required this.served, required this.server});
 
   final String mealName;
+  final String server;
   final bool served;
 
   @override
@@ -66,10 +64,13 @@ class _MealStatusState extends State<MealStatus> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(widget.mealName),
+        if (widget.served) ...[Text("Server: ${widget.server}")],
         Checkbox(
           checkColor: Colors.white,
           value: widget.served,
-          onChanged: (value) => {},
+          onChanged: (value) => {
+            
+          },
         )
       ],
     );
@@ -86,6 +87,91 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class LabeledRadio extends StatelessWidget {
+  const LabeledRadio({
+    super.key,
+    required this.label,
+    required this.padding,
+    required this.groupValue,
+    required this.value,
+    required this.onChanged,
+  });
+
+  final String label;
+  final EdgeInsets padding;
+  final String groupValue;
+  final String value;
+  final ValueChanged<String> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        if (value != groupValue) {
+          onChanged(value);
+        }
+      },
+      child: Padding(
+        padding: padding,
+        child: Row(
+          children: <Widget>[
+            Radio<String>(
+              groupValue: groupValue,
+              value: value,
+              onChanged: (String? newValue) {
+                onChanged(newValue!);
+              },
+            ),
+            Text(label),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MealSelectorRadio extends StatefulWidget {
+  const MealSelectorRadio({super.key});
+
+  @override
+  State<MealSelectorRadio> createState() => _MealSelectorRadioState();
+}
+
+class _MealSelectorRadioState extends State<MealSelectorRadio> {
+  String _selectedMeal = "Breakfast";
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <LabeledRadio>[
+        LabeledRadio(
+          label: 'Breakfast',
+          padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          value: 'Breakfast',
+          groupValue: _selectedMeal,
+          onChanged: (String newValue) {
+            setState(() {
+              _selectedMeal = newValue;
+            });
+          },
+        ),
+        LabeledRadio(
+          label: 'Dinner',
+          padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          value: 'Dinner',
+          groupValue: _selectedMeal,
+          onChanged: (String newValue) {
+            setState(() {
+              _selectedMeal = newValue;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
@@ -98,34 +184,49 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [Text(widget.title, textAlign: TextAlign.center)]),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
                 'Today is ${widget.now.year}-${widget.now.month}-${widget.now.day}',
                 style:
                     const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            const MealStatusBar(),
-            Image.asset("assets/images/juni-1.jpg"),
+            const Padding(padding: EdgeInsets.all(15)),
+            const MealStatusBar(breakfastServed: false, dinnerServed: false,),
+            Image.asset("assets/images/juni-1.jpg", width: 200, height: 300),
             ElevatedButton.icon(
               onPressed: () {
-                // Respond to button press
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        scrollable: true,
+                        content: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Form(
+                            child: Column(
+                              children: <Widget>[
+                                TextFormField(
+                                  decoration: const InputDecoration(
+                                    labelText: 'Feeder Name:',
+                                    icon: Icon(Icons.account_box),
+                                  ),
+                                ),
+                                const Padding(padding: EdgeInsets.all(15), child: Text("What meal are you feeding?")),
+                                const MealSelectorRadio(),
+                              ],
+                            ),
+                          ),
+                        ),
+                        actions: [
+                          ElevatedButton(
+                              child: const Text("Done"),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              })
+                        ],
+                      );
+                    });
               },
               icon: const Icon(Icons.fastfood_outlined, size: 18),
               label: const Text("I'm about to feed Juni"),
